fb5b874b887dd56ef4698af909c32803974de97b
AmolAmrit <amol.amrit99@gmail.com>
2017-01-07 20:09:31 +0000

    Add Miracast Support

--- msm8956.mk
+++ msm8956.mk
@@ -253,3 +253,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+# miracast props
+PRODUCT_PROPERTY_OVERRIDES += \
+   persist.debug.wfd.enable=1
