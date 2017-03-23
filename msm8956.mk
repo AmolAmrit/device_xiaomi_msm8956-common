5887eb248aee51a351ba825ccab7299f302c36b4
AmolAmrit <amol.amrit99@gmail.com>
2017-03-23 10:12:03 +0000

    msm8956: Disable Gello
    Signed-off-by: AmolAmrit <amol.amrit99@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -102,8 +102,8 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
 # Browser
-PRODUCT_PACKAGES += \
-    Gello
+#PRODUCT_PACKAGES += \
+   # Gello
 
 # Camera
 PRODUCT_PACKAGES += \
