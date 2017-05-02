b9f4292a6b4c3c30b169349f9466f0c49c20273a
TheScarastic <warabhishek@gmail.com>
2017-05-02 08:55:23 +0000

    msm8956: Add aanc_tuning_mixer_wcd9335
    Fixes:
    E MCS-RT-CTL: Can't open the configuration file /system/etc/aanc_tuning_mixer_wcd9335.txt.

--- msm8956.mk
+++ msm8956.mk
@@ -91,6 +91,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer_wcd9335.txt:system/etc/aanc_tuning_mixer_wcd9335.txt \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
