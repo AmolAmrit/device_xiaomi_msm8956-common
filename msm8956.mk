a9c317bc14c086433dafd0e05acaef60bb038e4d
TheScarastic <warabhishek@gmail.com>
2017-09-16 18:41:17 +0530

    msm8956: build hidl hal

--- msm8956.mk
+++ msm8956.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
 
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
