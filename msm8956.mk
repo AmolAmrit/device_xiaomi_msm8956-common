fa68d471020fcdf57a073911ea5083f3675b5cf8
TheScarastic <warabhishek@gmail.com>
2017-09-16 18:22:27 +0530

    msm8956: Build Health hal
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -206,6 +206,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-service \
+    android.hardware.health@1.0
 
 # IPA Manager
 #PRODUCT_PACKAGES += \
