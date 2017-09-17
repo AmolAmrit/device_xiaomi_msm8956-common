eb832058f510b236c50eac3620076f6a1c49f6ff
Adarsh-MR <adarshmr1998@gmail.com>
2017-09-17 16:28:10 +0000

    msm8956-common: Build Camera HAL
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -115,6 +115,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.msm8952 \
+    libqomx_core \
+    libmm-qcamera \
     camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl \
     Snap
