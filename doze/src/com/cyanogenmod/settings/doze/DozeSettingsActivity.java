9ecfba5b3d1cfdbc3ff2811858b45aff09117012
Adarsh-MR <adarshmr1998@gmail.com>
2017-10-20 11:30:46 +0000

    msm8956: Squashed bringup of AOSP Doze
    -All thanks to @AdarshMR
    
    msm8956-common: doze: take it out of cmsdk
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    msm8956-common: doze: Re-integrate into Settings
    
    * Apply the default Oreo theme and inherit the layouts
      from Google for the Settings app, in order to keep
      UI consistency.
    
    * Get rid of SettingsDrawerActivity as it no longer fits in
      and include the back button in the action bar.
    
    * Kill the icon drawable not only because is a leftover,
      but also doesn't really serve any puropose in the new UI.
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    msm8956-common: doze: Build with AAPT2
    
    msm8956-common: doze: Refactor
     * Fix Ambient Display's switchbar on/off text.
     * Get rid of unnecessary receiver and all its related code,
       permissions and meta-data.
     * Keep the service running only if Doze is enabled system-wide along
       with at least one of the additional Ambient Display features.
    
    Change-Id: Id1f7f5e099f289a20f5757b7869e297e0ce74f7a
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    msm8956-common: doze: move to display settings
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    msm8956-common: doze: remove custom layouts
    
    * devicesettings resource has it now
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    msm8956-common: properly integrate into oreo settings
    
    * rename string to Ambient display gestures
    
    msm8956-common: XiaomiDoze: Protect com.android.systemui.doze.pulse
    
    As stated in ActivityManagerService:
    
      The vast majority of broadcasts sent from system internals
      should be protected to avoid security holes
    
    Change-Id: I1dc989d9d132d40835ca8dbf277285eb88e30a58
    
    op3: Doze: Perform operations as current user
    
     * System processes should use the 'AsUser' variant of some methods
    
    Change-Id: Ic2457950b1f0e9f29f4fb06ef38f00dd919ad8f7
    
    msm8956-common: doze: Cleanup
    
     * Remove unneccessary newlines
     * Remove redundancy
     * Remove unused variables and imports
     * Make variables local when sufficient
    
    Change-Id: I48f5dfc24275388f1deb31dc27716ed75aae898e
    
    msm8956-common: doze: Set different timings for wave and pocket detection
    
     * One second is a little bit short for pocket
    
    Change-Id: Id27bba6f01a3a4d54ae9096fae87505f38ee5062
    
    msm8956-common: clean-up XiaomiDoze
    
    * Add back show help dialog
    * fix onchange for sensor toggles
    * remove unecessary permissions
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- doze/src/com/cyanogenmod/settings/doze/DozeSettingsActivity.java
+++ doze/src/com/cyanogenmod/settings/doze/DozeSettingsActivity.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +16,10 @@
  */
 package com.cyanogenmod.settings.doze;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.preference.PreferenceActivity;
 
-import com.android.settingslib.drawer.SettingsDrawerActivity;
-
-/**
- * Created by shade on 10/14/16.
- */
-
-public class DozeSettings extends SettingsDrawerActivity {
+public class DozeSettingsActivity extends PreferenceActivity  {
 
     private static final String TAG_DOZE = "doze";
 
@@ -33,18 +27,7 @@ public class DozeSettings extends SettingsDrawerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.doze);
-
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new DozeSettingsFragment(), TAG_DOZE).commit();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return false;
     }
 }
