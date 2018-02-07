e6449547eb3fac1b9d7a8c0471e1e2a4d36b143c
Joey <joey@lineageos.org>
2018-02-07 19:04:19 +0530

    msm8956: Build textclassifier
    Change-Id: I50d8173d24c4cab773824107b98929dbbc469783

--- msm8956.mk
+++ msm8956.mk
@@ -308,6 +308,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
