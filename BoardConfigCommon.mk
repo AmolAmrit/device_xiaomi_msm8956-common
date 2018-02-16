1e2ebce0de2449a83813c95b5e535024e96ece5f
TheScarastic <warabhishek@gmail.com>
2018-02-16 21:09:46 +0000

    msm8956-common: Build libaudioalsa as prebuilt for audio hal
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,7 +18,6 @@
 VENDOR_PATH := device/xiaomi/msm8956-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
-ALLOW_MISSING_DEPENDENCIES := true
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
