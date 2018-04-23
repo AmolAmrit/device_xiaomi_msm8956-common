a5fcb257ce4847f58149271a16439e2844e52dee
Michael Bestas <mkbestas@lineageos.org>
2018-04-23 20:42:35 +0300

    msm8956: QCamera2: Remove tests
    Change-Id: Ia1b7e2bb041b3849193ddefc749972caa45e8dae
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- camera/QCamera2/stack/Android.mk
+++ camera/QCamera2/stack/Android.mk
@@ -1,5 +1,1 @@
-LOCAL_PATH:= $(call my-dir)
-include $(LOCAL_PATH)/mm-camera-interface/Android.mk
-include $(LOCAL_PATH)/mm-jpeg-interface/Android.mk
-include $(LOCAL_PATH)/mm-jpeg-interface/test/Android.mk
-include $(LOCAL_PATH)/mm-camera-test/Android.mk
+include $(call all-subdir-makefiles)
