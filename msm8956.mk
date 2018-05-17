d5617c212c10f526cbb1a1b4189b219f54c1aff1
Himanshu Agrawal <hagraw@codeaurora.org>
2018-05-17 17:24:14 +0530

    msm8956: Changing healthd version to 2.0
    Change-Id: I13ddaa133bb9edcca200d5fe7320cf5fa9f66f0f
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -217,10 +217,9 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-service \
-    android.hardware.health@1.0 \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0 \
     chargeonlymode
 
 # HIDL
