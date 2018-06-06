8f1d65e42d11262a4a06556a012672bf92e1adbc
AmolAmrit <amol.amrit03@outlook.com>
2018-06-06 22:09:40 +0530

    msm8956: Cleanup!
    -Why I did not do this. Was I dreaming?
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -17,7 +17,7 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/msm8956-common/msm8956-common-vendor.mk)
