b7b2d5822518fa068884b090690da6ee7226fbbb
AmolAmrit <amol.amrit03@outlook.com>
2018-07-05 19:49:24 +0530

    msm8956: Don't DexPreOpt pre-builts *It is not recommended
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,7 +115,8 @@ ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true 
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+      DONT_DEXPREOPT_PREBUILTS := true 
     endif
   endif
 endif
