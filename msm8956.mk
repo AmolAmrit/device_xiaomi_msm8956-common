81c25f8aa3cde06f96b81316dcb695995e96a83f
Vladimir Oltean <olteanv@gmail.com>
2018-10-22 19:16:19 +0000

    msm8956-common: Build camera.provider@2.4-external-service
    * Fixes camera starting delays
    
    * Original commit:
     https://review.lineageos.org/#/c/LineageOS/android_device_lenovo_YTX703-common/+/230201/
     vintf: switch hwcomposer and camera HIDL HALs to binderized
    
    * What do the hwcomposer and camera HALs have in common?
      The libqservice library, part of the legacy qcom display HAL stack.
      This library registers the display.qservice with the service manager,
      which is then also accessed through Binder by the camera HAL.
    * The entity who links with libqservice is actually linking with it
      indirectly, since it is a direct dependency of the legacy hwcomposer.
    * Via the hwc2on1adapter, the process that actually hosts the
      libqservice code is the surfaceflinger.
    * The above is true when the hwcomposer HAL is same-process. This leads
      to the following SELinux denial:
    
      avc: denied { add find } for service=display.qservice scontext=u:r:surfaceflinger:s0
           tcontext=u:object_r:default_android_service:s0 tclass=service_manager
      E VendorServiceManager: add_service('display.qservice',1) uid=1000 - PERMISSION DENIED
    
    * This means that surfaceflinger, via the same-process hwcomposer HAL,
      is trying (and failing) to register display.qservice with the *system*
      service manager (/dev/binder). Note that due to the service being
      loaded by surfaceflinger (a system process which doesn't see
      /dev/vndbinder), even the security context is invalid
      (device/qcom/sepolicy-legacy/common/vndservice_contexts defines it
      as qdisplay_service).
    * Naturally, it is not a good idea to grant the above SELinux permission.
      Instead, by running the hwcomposer HAL in its own vendor daemon
      process, display.qservice can be correctly started as a vendor service
      and visible in $(vndservice list).
    * However, now a new problem arises: the camera HAL (via the camera
      server process) is no longer able to access display.qservice, since
      they belong to different Binder domains. The solution to this is to
      switch the camera HAL to binderized as well.
    
    Change-Id: I6408ceb44136b4924589536a7a012a6fac68e065
    Based on a discussion with: Łukasz Patron (LuK1337 <priv.luk@gmail.com>)
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -127,6 +127,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-external-service \
     camera.device@3.2-impl \
     camera.msm8952 \
     libqomx_core \
