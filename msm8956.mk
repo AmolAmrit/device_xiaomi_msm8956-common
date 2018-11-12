23b9d3fc0d6b390d53cf14fcf6a45e5061947852
Bruno Martins <bgcngm@gmail.com>
2018-11-12 21:18:12 +0000

    msm8956: Build ClearKey HIDL plugin from source
     * After all it can be built from source.
    
    Change-Id: Ie0752b3312007c94bc7dd8730fa88bd324a2ac8c
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -174,7 +174,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Doze mode
 PRODUCT_PACKAGES += \
