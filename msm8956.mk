9ca7e0942787ac398d356c1191a193e505f8bacc
AmolAmrit <amol.amrit03@outlook.com>
2018-12-07 20:50:22 +0530

    msm8956: Boostframework: Remove Nougat implementation from device tree
    Revert "msm8956: Boostframework: Include boostframework files"
    
    This reverts commit e9c2244c9e162655eb19a2f1e6424adce9544891.
    
    Revert "msm8956: Boostframework: Set some SEPolicy rules for perfd, camera daemon, etc."
    
    This reverts commit aaa9c27eb6003db568ea758437ed7f0e306e3625.
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    
    Revert "msm8956: Boostframework: Update ramdisk"
    
    This reverts commit 8118065c199bb69cef379e7d1f61253168f029a8.
    
    Revert "msm8956-common: Add QPerformance.jar to boot jars and pinner service"
    
    This reverts commit f922b6f1fcb6729a17b98979e9c177ad45d065a9.
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    
    msm8956: Remove Boostframework overlays
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -303,9 +303,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
-# QPerformance
-PRODUCT_BOOT_JARS += QPerformance
-
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
     libtinyxml \
