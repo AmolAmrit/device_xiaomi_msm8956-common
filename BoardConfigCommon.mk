9b7fb22b45a54f69e94803167af208c949f9a686
binzhang <binzhang@codeaurora.org>
2018-12-09 07:57:48 +0000

    msm8956: SEPolicy: Add related sepolicy for perfservice
    Change-Id: Iac48ed0a31c9b337a784274c88cc88a070b117bf
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -223,6 +223,7 @@ TARGET_LD_SHIM_LIBS := \
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
