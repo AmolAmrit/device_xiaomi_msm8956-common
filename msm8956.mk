8593b148dfa013fb082ed80022f4f39e256084ec
Bruno Martins <bgcngm@gmail.com>
2018-12-30 19:58:56 +0000

    msm8956-common: Convert XiaomiDoze into XiaomiParts
     * This is the preparation for turning the package into
       an unified device-settings app.
    
    Change-Id: I425ad525edcc63c2dcccd1e2fb8459f442744970
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -79,9 +79,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Ambient Display
+# Device-specific Settings
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    XiaomiParts
 
 
 # Audio
