7f0d4ffa6b37da56c7864a549f6eda19d20cd88c
Jiyong Park <jiyong@google.com>
2019-09-04 08:18:22 -0700

    msm8956-common: TARGET_FLATTEN_APEX=true for Nexus 6P
    APEXes are flattened for Nexus 6P. Kernel is too old to support APEXes
    without performance regression.
    
    Bug: 123684826
    Change-Id: Idf54b059be056b353caae3fec276055f6a78eab7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,6 +61,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8956
 
 # APEX image
 DEXPREOPT_GENERATE_APEX_IMAGE := true
+TARGET_FLATTEN_APEX := true
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
