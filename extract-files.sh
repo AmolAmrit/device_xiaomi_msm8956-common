5c92ad58a676c50788c0d26b7cae080ec5a0848e
Bruno Martins <bgcngm@gmail.com>
2019-10-13 23:08:59 +0530

    msm8956-common: Make QTI BT HAL load with VNDK 28 libbase
    Symbol "_ZN7android4base10LogMessageC1EPKcjNS0_5LogIdENS0_11LogSeverityEi"
    is referenced by this HAL impl blob. Ship a copy of the prebuilt VNDK 28
    version of libbase and make the HAL load it.
    
    Change-Id: I7f576739f5bd2fb9bdcaa07198ba2020bf15919c
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- extract-files.sh
+++ extract-files.sh
@@ -68,5 +68,6 @@ COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $COMMON_BLOB_ROOT/vendor/bin/slim_daemon
 patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $COMMON_BLOB_ROOT/lib64/vendor.qti.gnss@1.0.so
 patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $COMMON_BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
+patchelf --replace-needed libbase.so libbase-v28.so $COMMON_BLOB_ROOT/vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so
 
 "$MY_DIR"/setup-makefiles.sh
