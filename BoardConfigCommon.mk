2b058a3ba5363c01c0f7fa26d64a3ca3ef85cf3b
Akash Mondal <a98mondal@gmail.com>
2019-10-13 22:58:34 +0530

    msm8956-common: Create extra root folder for persist mount point
    * /mnt/vendor/persist is bind mounted on /persist
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,6 +158,7 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
 TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 
+BOARD_ROOT_EXTRA_FOLDERS := persist
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware
