79e87babaa5945cbbf44cd7873711e1490d87af0
AmolAmrit <amol.amrit03@outlook.com>
2020-01-06 19:50:48 +0530

    msm8956: SELinux: Enforce policy
    * Remove redudant legacy inclusion
    * Special thanks to my friend @Adarsh-MR for all the work he has done. Miss you man.
    * Huge thanks for testing, fixing and finalising all the work @Abhi011999 [@BoogeyWoogey69 on Telegram]. Thank you for investing your time.
    * Thank you @amanrajOO7 for all the work.
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 ramoops_memreserve=4M
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
@@ -219,11 +219,10 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib64/lib-imsvideocodec.so|libshim_ims.so
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#include device/qcom/sepolicy-legacy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
+include device/qcom/sepolicy-legacy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
