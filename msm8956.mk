e062207817bb0d53bf06832204c012a18cf7ed8b
Magicxavi <rolfpeterkesche@gmail.com>
2020-02-10 18:49:14 +0200

    msm8956-common: Add spectrum support
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -348,7 +348,9 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.qcom.usb.rc \
     ueventd.qcom.rc \
-    init.safailnet.rc
+    init.safailnet.rc \
+    init.spectrum.rc \
+    init.spectrum.sh
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
