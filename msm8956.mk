72fd75ec075f922d1fed1dd26f1fa126e16b92ca
AmolAmrit <amol.amrit03@outlook.com>
2020-05-09 22:21:34 +0530

    msm8956-common: V
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- msm8956.mk
+++ msm8956.mk
@@ -407,8 +407,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
-
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Widevine
 PRODUCT_PACKAGES += \
