0b39e7222d5a703fee9dafc914ffdb56a6a88790
AmolAmrit <amol.amrit03@outlook.com>
2020-08-17 14:18:15 +0530

    msm8956-common: Don't build SEPolicy
    * Remove SELinux context
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -219,10 +219,10 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib64/lib-imsvideocodec.so|libshim_ims.so
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
+#include device/qcom/sepolicy-legacy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
